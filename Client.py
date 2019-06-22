5ecf682265fe3d89efa6fba13be65aed91b2b408
Samuel C. M. Siqueira <44274148+samuelcms@users.noreply.github.com>
2019-06-22 12:57:03 -0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -7,10 +7,10 @@ import os
 
 # Inicializando os parâmetros da biblioteca pyaudio.
 
-CHUNK = 1024 # Número de quadros no buffer.
+CHUNK = 1024    # Número de quadros no buffer.
 FORMAT = pyaudio.paInt16
-CHANNELS = 1 # Cada quadro tem 1 amostra ("CHANNELS = 1)
-RATE = 52250 # # Número de amostras coletadas por segundo.
+CHANNELS = 1    # Cada quadro tem 1 amostra ("CHANNELS = 1)
+RATE = 52250    # Número de amostras coletadas por segundo.
 
 # Inicializando sockets.
 Servidor = '127.0.0.1'
@@ -22,7 +22,6 @@ voz = pyaudio.PyAudio()
 
 stream = voz.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
-# - Criar threading para gerenciar conexao e fechamento - 
 os.system('clear')
 print("\n\t*Gravando*n\n")
 print("Para interromper a ligação, pressione Ctrl + C.")
