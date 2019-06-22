b3574f44b21f8cb352392ad8b586b02497dca476
Samuel C. M. Siqueira <44274148+samuelcms@users.noreply.github.com>
2019-06-21 21:07:20 -0300

    Add files via upload

--- Server.py
+++ Server.py
@@ -8,38 +8,36 @@ import os
 
 # Inicializando os parâmetros da biblioteca pyaudio.
 
-CHUNK = 1024 # Número de quadros no buffer.
-#FORMAT = pyaudio.paInt16
-CHANNELS = 1 # Cada quadro tem 1 amostra ("CHANNELS = 1)".
-RATE = 52250 # Número de amostras coletadas por segundo.
+CHUNK = 2048 * 5    # Número de quadros no buffer.
+CHANNELS = 1        # Cada quadro tem 1 amostra ("CHANNELS = 1)".
+RATE = 52250        # Número de amostras coletadas por segundo.
 WIDTH = 2
 frames = []
 
-p = pyaudio.PyAudio() #Função para calcular o tamanho de cada amostra.
+p = pyaudio.PyAudio() # Configura o sistema de "portaudio".
 
-stream = p.open(format=p.get_format_from_width(WIDTH),
-                channels=CHANNELS,
-                rate=RATE,
-                output=True,
-                frames_per_buffer=CHUNK)
+stream = p.open(format=p.get_format_from_width(WIDTH), channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
 
+HOST = '127.0.0.1'                 
+PORT = 29000              
 
-HOST = ''                 
-PORT = 22000              
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
+
 conn, addr = s.accept()
 print ('Conexão vinda de: ', addr)
-data = conn.recv(CHUNK) #Dados
+
+data = conn.recv(CHUNK) # Dados
 
 i=1
-while data != '': #Enquanto os dados forem diferentes de NULL.
+
+while data != '': # Enquanto os dados forem diferentes de NULL.
     stream.write(data)
-    data = conn.recv(1024)
+    data = conn.recv(CHUNK)
     frames.append(data)
-    i = i+1
-    print(i)
+    #i = i+1
+    #print(i)
     #os.system('clear')
  
 stream.stop_stream()
