7ad2366b95678ee2cabeb4b0bd8e8b8e03f757a0
Samuel C. M. Siqueira <44274148+samuelcms@users.noreply.github.com>
2019-06-22 15:45:39 -0300

    Add files via upload

--- Server.py
+++ Server.py
@@ -14,13 +14,14 @@ RATE = 52250        # Número de amostras coletadas por segundo.
 WIDTH = 2
 frames = []
 
-p = pyaudio.PyAudio() # Configura o sistema de "portaudio".
+p = pyaudio.PyAudio()   # Configura o sistema de "portaudio".
 
 stream = p.open(format=p.get_format_from_width(WIDTH), channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
 
+# Inicializando sockets.
+
 HOST = '127.0.0.1'                 
 PORT = 29000              
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
@@ -28,20 +29,20 @@ s.listen(1)
 conn, addr = s.accept()
 print ('Conexão vinda de: ', addr)
 
-data = conn.recv(CHUNK) # Dados
+data = conn.recv(CHUNK)     # Dados.
 
-i=1
+contador = 1    # Conta as interações.
 
-while data != '': # Enquanto os dados forem diferentes de NULL.
+while data != '':       # Enquanto os dados forem diferentes de NULL.
     stream.write(data)
     data = conn.recv(CHUNK)
     frames.append(data)
-    #i = i+1
-    #print(i)
+    #contador = contador + 1
+    #print(contador)
     #os.system('clear')
  
 stream.stop_stream()
 stream.close()
 p.terminate()
 conn.close()
-s.close() # Fechando a conexão socket.
+s.close()           # Fechando a via conexão socket.
