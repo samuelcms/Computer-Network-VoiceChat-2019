eaf0583a757b3d62a892551716715c21529059c3
Samuel C. M. Siqueira <44274148+samuelcms@users.noreply.github.com>
2019-06-29 21:26:49 -0300

    Add files via upload

--- Com ACK/Client.py
+++ Com ACK/Client.py
@@ -25,16 +25,16 @@ FORMAT = pyaudio.paInt16    # Formato das amostras de áudio.
 CHANNELS = 1                # Cada quadro tem 1 amostra ("CHANNELS = 1)
 RATE = 52250                # Número de amostras coletadas por segundo.
 
-# Inicializando o socket de dados.
+# Criando conexão de dados.
 
 SERVER = 'localhost'        
-PORT = 16000                
+PORT = 20000                
 skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 skt.connect((SERVER, PORT))
 
-# Inicializando o socket de controle.
+# Criando conexão de controle.
 
-CTRL_PORT = 61000   
+CTRL_PORT = 40000   
 ctrl_skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ctrl_skt.connect((SERVER, CTRL_PORT))
 
@@ -43,29 +43,40 @@ voz = pyaudio.PyAudio()
 # Inicialização da stream de áudio.
 stream = voz.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
-#os.system('clear')
-print("\n\tChamada em execução...\n")
-print("Para interromper a chamada, pressione 0 e tecle ENTER.")
+os.system('clear')
+print('\t\t -------------------------')
+print("\t\t|   Chamada em execução   |")   
+print('\t\t -------------------------\n')
 
-flag = '1' # Indica o estado da conexão.
+print("\n* Para interromper a chamada, pressione 0 e tecle ENTER.")
 
-# Envia audio enquanto a chamada não for finalizada.
+status = '1'                   # Indica o estado da conexão (0 = OFF | 1 = ON).
+
+# Envia os dados enquanto a chamada não for finalizada.
 while  opcao != '0':
     data  = stream.read(CHUNK)
-    frames.append(data)
     skt.sendall(data)
-    ctrl_skt.sendall(flag.encode('utf-8')) # Envia dados de controle.
+    ctrl_skt.sendall(status.encode('utf-8'))     # Envia dados de controle.
+
+# Fechamento da stream.
 
 stream.stop_stream()
 stream.close()
 voz.terminate()
 skt.close()
 
-# Dado de encerramento da conexão.
-flag = '0'
-ctrl_skt.sendall(flag.encode('utf-8'))
+status = '0'                                     # Confirmação do encerramento da conexão.
+ctrl_skt.sendall(status.encode('utf-8'))
 ctrl_skt.close()
 
-end = datetime.now() # Hora de finalização da chamada.
-print("Chamada encerrada em {}".format(end))
+# Data e hora de finalização da chamada.
 
+end = str(datetime.now())                        
+aux = end.split(' ')
+data = aux[0]
+hora = aux[1]
+data = data.split('-')
+hora = hora.split('.')
+end = "{}/{}/{} | {}".format(data[2],data[1],data[0],hora[0])
+os.system('clear')
+print(f"\n\n\tChamada encerrada em: {end}\n\n")
--- Com ACK/Server.py
+++ Com ACK/Server.py
@@ -14,52 +14,63 @@ CHUNK = 2048 * 5            # Número de quadros no buffer.
 FORMAT = pyaudio.paInt16    # Formato das amostras de áudio.
 CHANNELS = 1                # Cada quadro tem 1 amostra ("CHANNELS = 1)".
 RATE = 52000                # Número de amostras coletadas por segundo.
-WIDTH = 2
+WIDTH = 2                   # Largura das amostras de áudio (em bytes).
 
 # Criando conexão de dados.
 
 HOST = ''                 
-PORT = 16000              
+PORT = 20000              
 skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 skt.bind((HOST, PORT))
-skt.listen(1)
+skt.listen(0)
 
 # Criando conexão de controle.
 
-CTRL_PORT = 61000
+CTRL_PORT = 40000
 ctrl = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ctrl.bind((HOST, CTRL_PORT))
-ctrl.listen(1)
+ctrl.listen(0)
 
 def conexao(conn, addr, ctrl, addr2):  
     
-    voz = pyaudio.PyAudio()       # Configura o sistema de "portaudio".
+    voz = pyaudio.PyAudio()         # Configura o sistema de "portaudio".
     stream = voz.open(format=voz.get_format_from_width(WIDTH), channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
 
-    #os.system('clear')
+    os.system('clear')
     print ('Conexão vinda de:', addr)
-    data = conn.recv(CHUNK) #Dados
+    data = conn.recv(CHUNK)         # Dados
 
-    flag = '1' # Indica o estado da conexão.
+    status = '1'                    # Indica o estado da conexão.
 
-    # A mensagem de voz é reproduzida enquanto houverem dados a serem
-    # recebidos, e o cliente estiver conectado.
-    while data != '' and flag == '1':   
+    # Enquanto o cliente estiver conectado e os dados forem diferentes de NULL, a mensagem de voz será reproduzida.
+    
+    while data != '' and status == '1':   
         stream.write(data)
-        data = conn.recv(CHUNK)
-        
+        data = conn.recv(CHUNK)    
         aux = ctrl.recv(1)          # Recebimento do status de conexão.
-        flag = aux.decode('utf-8')
+        status = aux.decode('utf-8')
 
-    end = datetime.now()    # Hora de finalização da chamada.
+    end = datetime.now()            # Data e hora de finalização da chamada.
 
     # Fechamento da stream.
 
     stream.stop_stream()
     stream.close()
     voz.terminate()     
-    print('Chamada encerrada em {}'.format(end))
+    
+    # Data e hora de finalização da chamada.
 
+    end = str(datetime.now())                        
+    aux = end.split(' ')
+    data = aux[0]
+    hora = aux[1]
+    data = data.split('-')
+    hora = hora.split('.')
+    end = "{}/{}/{} | {}".format(data[2],data[1],data[0],hora[0])
+    os.system('clear')
+    print(f"Chamada encerrada em: {end}\n\n")
+
+    
 def main():
     while True:
         conn, addr = skt.accept()
